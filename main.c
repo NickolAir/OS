17a08fcfed7cb6784498314607e7512eca962a6e
NickolAir <nikolay.ratushnyak03@gmail.com>
2023-03-18 14:11:14 +0700

    reversed dir and file names

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <string.h>
 
-char strrev(char *S) {
+char *strrev(char *S) {
     int i,j,l;
     char t;
     l = strlen(S);
@@ -19,7 +19,7 @@ char strrev(char *S) {
         i++;
         j--;
     }
-    return *S;
+    return S;
 }
 
 int get_length(char *path) {
@@ -47,7 +47,8 @@ int main(int argc, char *argv[]) {
     struct dirent *entry;
     char *path = argv[1];
     int name_len;
-    char *rev_name = (char*) malloc(sizeof(char));
+    char *file_path = (char*) malloc(sizeof(char));
+    char *new_file_path = (char*) malloc(sizeof(char));
     char *new_path = (char*) malloc(strlen(path) * sizeof(char));
     strcpy(new_path, path);
 
@@ -60,16 +61,34 @@ int main(int argc, char *argv[]) {
     name_len = get_length(path);
     rev_dir(path, new_path, name_len);
     printf("%s\n", new_path);
-
-    rev_name = (char*) realloc(rev_name, sizeof(char) * name_len);
+    //mkdir(new_path, 0755);
 
     while ((entry = readdir(dir)) != NULL) {
+        //regular files have type 8
         if (entry->d_type == 8) {
+            name_len = strlen(entry->d_name);
+            file_path = (char*) realloc(file_path, sizeof(char) * (strlen(path) + name_len + 1));
+            strcpy(file_path, path);
+            strcat(file_path, "/");
+            strcat(file_path, entry->d_name);
+            printf("%s\n", file_path);
+
+            new_file_path = (char*) realloc(new_file_path, sizeof(char) * (strlen(path) + name_len + 1));
+            strcpy(new_file_path, new_path);
+            strcat(new_file_path, "/");
+            char *rev_file_name = entry->d_name;
+            strcat(new_file_path, strrev(rev_file_name));
+            printf("%s\n", new_file_path);
+
             printf("inode: %d, name: %s, type: [%d], length: %d\n",
                    entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
         }
     }
 
     closedir(dir);
+
+    free(file_path);
+    free(new_path);
+    free(new_file_path);
     return 0;
 }
