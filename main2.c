a67e8ca3859862b233df7cccccdea68934babb26
NickolAir <nikolay.ratushnyak03@gmail.com>
2023-03-26 21:42:50 +0700

    operations with dirs and files

--- main2.c
+++ main2.c
@@ -4,6 +4,23 @@
 #include <sys/stat.h>
 #include <string.h>
 
+char *get_name(char *path) {
+    int i = strlen(path);
+    int count = 0;
+    int index = 0;
+    while (path[i] != '/'){
+        count++;
+        i--;
+    }
+
+    char *name = (char*) malloc(count * sizeof(char));
+    for (int j = i + 1; j < i + count; ++j) {
+        name[index] = path[j];
+        index++;
+    }
+    return name;
+}
+
 void make_dir(char *path) {
     mkdir(path, ACCESSPERMS);
 }
@@ -28,6 +45,36 @@ void remove_dir(char *path) {
     remove(path);
 }
 
+void create_file(char *path){
+    FILE *fd;
+    fd = fopen(path, "w");
+    fclose(fd);
+}
+
+void print_file(char *path) {
+    FILE *fd;
+    char *buf = (char *)malloc(BUFSIZ * sizeof(char));
+    fd = fopen(path, "r");
+    if (fd == NULL) {
+        printf("Failed to open file\n");
+        exit(-1);
+    }
+
+    while(fd != EOF) {
+        fgets(buf, BUFSIZ, fd);
+        puts(buf);
+    }
+
+    free(buf);
+    fclose(fd);
+}
+
+void remove_file(char *path) {
+    remove(path);
+}
+
 int main(int argc, char *argv[]) {
+    char *name = get_name(argv[0]);
+    free(name);
     return 0;
 }
